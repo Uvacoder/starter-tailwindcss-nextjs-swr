2fecde1c9cce0a28ab3c5f7efdd10a3c7f25c1f9
uvacoder <uva22940coder@gmail.com>
2022-11-02 10:57:07 -0400

    Initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
+
+export default class Document extends NextDocument {
+  static getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
